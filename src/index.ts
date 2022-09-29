0ce9e424aff44c10d1275ae524252c50e72cad93
snipsy <snipsy@rick-roll.me>
2022-09-29 08:55:37 +0200

    update to the structure of the bot

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,9 @@
-import { Client } from 'discord.js';
-import { loadEnv } from './utils/functions';
+import { TestBotClient } from './structures/clients/TestBotClient';
 
 declare global {
     type int = number;
 }
 
+const client = new TestBotClient();
 
-const env = loadEnv();
-
-const client = new Client({
-    intents: [
-        'Guilds',
-        'GuildMembers',
-        'GuildBans',
-        'GuildEmojisAndStickers',
-        'GuildIntegrations',
-        'GuildWebhooks',
-        'GuildInvites',
-        'GuildVoiceStates',
-        'GuildPresences',
-        'GuildMessages',
-        'GuildMessageReactions',
-        'GuildMessageTyping',
-        'DirectMessages',
-        'DirectMessageReactions',
-        'DirectMessageTyping',
-        'MessageContent',
-        'GuildScheduledEvents'
-
-    ]
-});
-
-
-client.login(env.DISCORD_TOKEN);
+client.login();
